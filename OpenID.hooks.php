wurstmineberg/mediawiki-extensions-OpenID

commit 61d5807ea49252683c04408c34913952049a26c8

Use $wgExtensionAssetsPath not $wgScriptPath . '/extensions/<extensionname>'
Fixes broken image in "Your OpenID for login to other sites" in preferences due to a non standard extension path

Change-Id: I37392d548285083e1231ffed2578cef8100aa796

OpenID.php
@@ -46,7 +46,7 @@ $wgOpenIDHideOpenIDLoginLink = false;
  * Location of the OpenID login logo. You can copy this to your server if you
  * want.
  */
-$wgOpenIDLoginLogoUrl = $wgScriptPath . '/extensions/OpenID/skin/icons/openid-inputicon.png';
+$wgOpenIDLoginLogoUrl = $wgExtensionAssetsPath . '/OpenID/skin/icons/openid-inputicon.png';
 
 /**
  * Whether to show the OpenID identity URL on a user's home page. Possible

commit 8c56257d6f7e4f28d1a9b49988f4cafaea15de1b

bug 46006 corrected the use of $wgExtensionAssetsPath after setup
changed wgOpenIDLoginLogoUrl to wgOpenIDSmallLogoUrl
version 3.06 20130312

this fix corrects a regression from 61d5807ea49252683c04408c34913952049a26c8

Change-Id: I03cece34f2c6f9ee2a8dc158f3d45c10baf74cce

OpenID.hooks.php
@@ -18,7 +18,9 @@ class SpecialOpenIDUserLogin extends SpecialRedirectToSpecial {
 
 class OpenIDHooks {
 	public static function onSpecialPage_initList( &$list ) {
-		global $wgOpenIDOnly, $wgOpenIDClientOnly, $wgSpecialPageGroups;
+		global $wgOpenIDOnly, $wgOpenIDClientOnly, $wgSpecialPageGroups, $wgOpenIDSmallLogoUrl;
+
+		$wgOpenIDSmallLogoUrl = self::getOpenIDSmallLogoUrl();
 
 		if ( $wgOpenIDOnly ) {
 			$list['Userlogin'] = 'SpecialOpenIDLogin';
@@ -128,9 +130,7 @@ class OpenIDHooks {
 	 * @return string
 	 */
 	private static function getAssociatedOpenIDsTable( $user ) {
-		global $wgLang, $wgOpenIDLoginLogoUrl;
-
-		$openIDLogo = Xml::element( 'img', array( 'src' => $wgOpenIDLoginLogoUrl, 'alt' => 'OpenID' ), '' );
+		global $wgLang;
 
 		$openid_urls_registration = SpecialOpenID::getUserOpenIDInformation( $user );
 		$delTitle = SpecialPage::getTitleFor( 'OpenIDConvert', 'Delete' );
@@ -153,7 +153,9 @@ class OpenIDHooks {
 			$rows .= Xml::tags( 'tr', array(),
 				Xml::tags( 'td',
 					array(),
-					$openIDLogo . "&nbsp;" . Xml::element( 'a', array( 'href' => $url_reg->uoi_openid ), $url_reg->uoi_openid )
+					self::getOpenIDSmallLogoUrlImageTag() .
+						"&nbsp;" .
+						Xml::element( 'a', array( 'href' => $url_reg->uoi_openid ), $url_reg->uoi_openid )
 				) .
 				Xml::tags( 'td',
 					array(),
@@ -261,7 +263,7 @@ class OpenIDHooks {
 	 */
 	public static function onGetPreferences( $user, &$preferences ) {
 		global $wgOpenIDShowUrlOnUserPage, $wgHiddenPrefs,
-			$wgAuth, $wgUser, $wgLang, $wgOpenIDOnlyClient, $wgOpenIDLoginLogoUrl;
+			$wgAuth, $wgUser, $wgLang, $wgOpenIDOnlyClient;
 
 		switch ( $wgOpenIDShowUrlOnUserPage ) {
 
@@ -333,14 +335,12 @@ class OpenIDHooks {
 
 		if ( !$wgOpenIDOnlyClient ) {
 
-			$openIDLogo = Xml::element( 'img', array( 'src' => $wgOpenIDLoginLogoUrl, 'alt' => 'OpenID' ), '' );
-
 			$preferences['openid-your-openid'] =
 				array(
 					'section' => 'openid/openid-local-identity',
 					'type' => 'info',
 					'label-message' => 'openid-local-identity',
-					'default' => $openIDLogo . "&nbsp;" . SpecialOpenIDServer::getLocalIdentityLink( $user ),
+					'default' => self::getOpenIDSmallLogoUrlImageTag() . "&nbsp;" . SpecialOpenIDServer::getLocalIdentityLink( $user ),
 					'raw' => true,
 				);
 
@@ -485,12 +485,36 @@ class OpenIDHooks {
 	/**
 	 * @return string
 	 */
+	private static function getOpenIDSmallLogoUrl() {
+		global $wgOpenIDSmallLogoUrl, $wgExtensionAssetsPath;
+
+		if ( !$wgOpenIDSmallLogoUrl ) {
+			return $wgExtensionAssetsPath . '/OpenID/skin/icons/openid-inputicon.png';
+		} else {
+			return $wgOpenIDSmallLogoUrl;
+		}
+
+	}
+
+	/**
+	 * @return string
+	 */
+	private static function getOpenIDSmallLogoUrlImageTag() {
+		return Xml::element( 'img',
+			array( 'src' => self::getOpenIDSmallLogoUrl(), 'alt' => 'OpenID' ),
+			''
+		);
+	}
+
+	/**
+	 * @return string
+	 */
 	private static function loginStyle() {
-		global $wgOpenIDLoginLogoUrl;
+		$openIDLogo = self::getOpenIDSmallLogoUrl();
 		return <<<EOS
 		<style type='text/css'>
 		li#pt-openidlogin {
-		  background: url($wgOpenIDLoginLogoUrl) top left no-repeat;
+		  background: url($openIDLogo) top left no-repeat;
 		  padding-left: 20px;
 		  text-transform: none;
 		}

OpenID.php
@@ -29,7 +29,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-define( 'MEDIAWIKI_OPENID_VERSION', '3.05 20130311' );
+define( 'MEDIAWIKI_OPENID_VERSION', '3.06 20130312' );
 
 $path = dirname( __FILE__ );
 set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . get_include_path() );
@@ -43,10 +43,10 @@ set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . g
 $wgOpenIDHideOpenIDLoginLink = false;
 
 /**
- * Location of the OpenID login logo. You can copy this to your server if you
- * want.
+ * Location (fully specified Url) of a small OpenID logo.
+ * When set to false (default), the built-in standard logo is used.
  */
-$wgOpenIDLoginLogoUrl = $wgExtensionAssetsPath . '/OpenID/skin/icons/openid-inputicon.png';
+$wgOpenIDSmallLogoUrl = false;
 
 /**
  * Whether to show the OpenID identity URL on a user's home page. Possible

OpenIDProvider.body.php
@@ -41,8 +41,7 @@ class OpenIDProvider {
 	private function getButtonHTML( $classSize ) {
 		global $wgOpenIDShowProviderIcons;
 
-		if ( $wgOpenIDShowProviderIcons )
-		{
+		if ( $wgOpenIDShowProviderIcons ) {
 			return '<a id="openid_provider_' . $this->id . '_icon" title="' . $this->name . '"' .
 			' href="javascript: openid.show(\'' . $this->id . '\');"' .
 			' class="openid_' . $classSize . '_btn' .
@@ -102,10 +101,14 @@ class OpenIDProvider {
 	 */
 	public static function getLargeProviders() {
 		return  array(
-			new self( 'openid', 'OpenID', wfMessage( 'openid-provider-label-openid' )->text(), '{URL}' ),
-			new self( 'google', 'Google', wfMessage( 'openid-provider-label-google' )->text(), 'https://www.google.com/accounts/o8/id' ),
-			new self( 'yahoo', 'Yahoo', wfMessage( 'openid-provider-label-yahoo' )->text(), 'http://yahoo.com/' ),
-			new self( 'aol', 'AOL', wfMessage( 'openid-provider-label-aol' )->text(), 'http://openid.aol.com/{username}' ),
+			new self( 'openid', 'OpenID', wfMessage( 'openid-provider-label-openid' )->text(),
+				'{URL}' ),
+			new self( 'google', 'Google', wfMessage( 'openid-provider-label-google' )->text(),
+				'https://www.google.com/accounts/o8/id' ),
+			new self( 'yahoo', 'Yahoo', wfMessage( 'openid-provider-label-yahoo' )->text(),
+				'http://yahoo.com/' ),
+			new self( 'aol', 'AOL', wfMessage( 'openid-provider-label-aol' )->text(),
+				'http://openid.aol.com/{username}' ),
 		);
 	}
 
@@ -116,21 +119,21 @@ class OpenIDProvider {
 	public static function getSmallProviders() {
 		return array(
 			new self( 'myopenid', 'MyOpenID', wfMessage( 'openid-provider-label-other-username', 'MyOpenID' )->text(),
-						'http://{username}.myopenid.com/' ),
+				'http://{username}.myopenid.com/' ),
 			new self( 'livejournal', 'LiveJournal', wfMessage( 'openid-provider-label-other-username', 'LiveJournal' )->text(),
-						'http://{username}.livejournal.com/' ),
+				'http://{username}.livejournal.com/' ),
 			new self( 'vox', 'VOX', wfMessage( 'openid-provider-label-other-username', 'VOX' )->text(),
-						'http://{username}.vox.com/' ),
+				'http://{username}.vox.com/' ),
 			new self( 'blogger', 'Blogger', wfMessage( 'openid-provider-label-other-username', 'Blogger' )->text(),
-						'http://{username}.blogspot.com/' ),
+				'http://{username}.blogspot.com/' ),
 			new self( 'flickr', 'Flickr', wfMessage( 'openid-provider-label-other-username', 'Flickr' )->text(),
-						'http://flickr.com/photos/{username}/' ),
+				'http://flickr.com/photos/{username}/' ),
 			new self( 'verisign', 'Verisign', wfMessage( 'openid-provider-label-other-username', 'Verisign' )->text(),
-						'http://{username}.pip.verisignlabs.com/' ),
+				'http://{username}.pip.verisignlabs.com/' ),
 			new self( 'vidoop', 'Vidoop', wfMessage( 'openid-provider-label-other-username', 'Vidoop' )->text(),
-						'http://{username}.myvidoop.com/' ),
+				'http://{username}.myvidoop.com/' ),
 			new self( 'claimid', 'ClaimID', wfMessage( 'openid-provider-label-other-username', 'ClaimID' )->text(),
-						'http://claimid.com/{username}' )
+				'http://claimid.com/{username}' )
 		);
 	}
 }

README.OpenID-mediawiki-extension
@@ -323,7 +323,7 @@ LocalSettings.php file. Please read carefully.
   the user navigates to Special:OpenIDLogin directly; it's simply
   cosmetic. This is mostly a backwards-compatibility option.
 
-* $wgOpenIDLoginLogoUrl -- Url of the OpenID login logo. Defaults to
+* $wgOpenIDSmallLogoUrl -- Url of the OpenID login logo. Defaults to
   'http://www.openid.net/login-bg.gif', but you may want to move it to
   a local URL, or an URL on a CDN, if that kind of thing floats your
   boat.

SpecialOpenID.body.php
@@ -50,9 +50,9 @@ class SpecialOpenID extends SpecialPage {
 			&& ( $wgOpenIDShowUrlOnUserPage === 'always'
 				|| ( ( $wgOpenIDShowUrlOnUserPage === 'user' ) && !$wgUser->getOption( 'openid-hide-openid' ) ) ) ) {
 
-			global $wgOpenIDLoginLogoUrl;
+			global $wgOpenIDSmallLogoUrl;
 			$wgOut->setSubtitle( "<span class='subpages'>" .
-				"<img src='$wgOpenIDLoginLogoUrl' alt='OpenID' />" .
+				"<img src='$wgOpenIDSmallLogoUrl' alt='OpenID' />" .
 				SpecialOpenIDServer::getLocalIdentityLink( $wgUser ) .
 				"</span>" );
 		}